817dffc7b3f3cffed0e79b26a319d407fb0b1c47
lilmx5 <161989540+lilmx5@users.noreply.github.com>
2025-12-01 16:33:31 -0800

    Updated Necessary Files

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("assignment2Btn").addEventListener("click", function() {
     window.location.href = "Assignment 2/index.html";
 })
+document.getElementById("assignment5Btn").addEventListener("click", function(){
+    window.location.href = "Assignment 5/fancifymytext.html";
+})
